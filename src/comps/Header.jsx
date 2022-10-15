9c1b71a93fa8f5ec1a8fac62964562fd83b8188f
Dean-The-Dream <dean.lawrence@icloud.com>
2022-10-15 12:12:32 -0400

    update: add bootstrap

---
+++ src/comps/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import react from '../assets/react.svg'
+
+function Header() {
+  return (
+    <>
+        <div>Header</div>
+        <img src={react} alt="" />
+    </>
+  )
+    
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
 import App from './App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
